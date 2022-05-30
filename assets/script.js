80877b489f3800e31d29db384e806b6feb8307cd
Lucas Saaler <lucassaaler00@gmail.com>
2022-05-30 13:25:05 -0400

    removed reset.css and fleshed out html skeleton. Also added some static selectors in script.js

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var highScoreEl = document.querySelector("#highscore")
+var timerEl = document.querySelector("#timeLeft")
+var startupEl = document.querySelector("#startup")
+var questionsEl = document.querySelector("#questions")
+var initialsEl = document.querySelector("#initials")
+var leaderboardEl = document.querySelector("#leaderboard")
+var startQuizEl = document.querySelector("#start-quiz")
+
+var timeRemaining = 75
+
+function countDown(){
+
+}
+
+function startQuiz(){
+
+}
+
+startQuizEl.addEventListener("click", startQuiz)
